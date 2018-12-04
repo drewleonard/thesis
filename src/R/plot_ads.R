93e402d317566e7eef5733e28775b145453e5c17
drewleonard <drew.n.leonard.19@dartmouth.edu>
2018-12-04 13:53:55 +0530

    coolplot node sizes

--- src/R/plot_ads.R
+++ src/R/plot_ads.R
@@ -505,27 +505,37 @@ nodes <- read.csv("csv/group_node.csv")
 
 nums <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52")
 
-
+group_sizes <- full %>% 
+  select('AccountGroup') %>% 
+  group_by(AccountGroup) %>% 
+  summarise(rank = n()) %>% 
+  ungroup() %>% 
+  filter(AccountGroup %in% colnames(cormatrix)) %>% 
+  rowwise() %>% 
+  mutate(rank = max((log2(rank)/1.5),1) + 2)
+  
 edges_spread <- edges %>%
   spread(group1, weight)
 edges_spread[is.na(edges_spread)] <- 0
 
 cormatrix <- cor_auto(edges_spread)
-graph1 <- qgraph(cormatrix,
-  graph = "glasso", layout = "spring", sampleSize = nrow(edges_spread),
-  vsize = 7, cut = 0, maximum = .45, border.width = 1.5,
-  labels = nums,
-  vsize = 2,
-  node.width = .51,
-  node.heigt = .51,
+coolplot <- qgraph(cormatrix,
+  graph = "glasso", 
+  layout = "spring", 
+  sampleSize = nrow(edges_spread),
+  cut = 0, 
+  maximum = .45, 
+  border.width = 1.5, 
+  labels = nums, 
   filetype = "pdf",
-  filename = "~/Documents/coolplot3",
+  filename = "~/Documents/coolplot",
   width = 15,
   height = 15,
   legend = TRUE,
   legend.mode = "names",
   nodeNames = colnames(cormatrix),
-  # XKCD = TRUE,
+  vsize = group_sizes$rank,
+  # XKCD = TRUE, # the most important argument
   theme = "Borkulo"
 )
 
