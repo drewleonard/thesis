12f6403e34695d14523f93e7ee21a3fe423842eb
drewleonard <drew.n.leonard.19@dartmouth.edu>
2018-12-08 10:59:09 +0530

    function to plot forest plots for primary topics groupings

--- src/R/plot_ads.R
+++ src/R/plot_ads.R
@@ -213,6 +213,79 @@ full %>%
   )
 dev.off()
 
+# Helper function for making forest plot
+formatInterval <- function(mean, lower, upper) {
+  upper <- formatC(upper, format = "f", digits = 3)
+  upper <- ifelse(grepl("-", upper), str_c("", upper), str_c("  ", upper))
+  lower <- formatC(lower, format = "f", digits = 3)
+  lower <- ifelse(grepl("-", lower), str_c(" ", lower), str_c("   ", lower))
+  interval <- str_c("[", lower, " , ", upper, " ]")
+  mean <- formatC(mean, format = "f", digits = 3)
+  return(str_c(mean, "   ", interval))
+}
+
+# Function for making forest plots
+makePrimaryTopicForestPlot <- function(plot, file_name, covar_label_1, covar_label_2) {
+  
+  plotDF <- data.frame(t(sapply(plot$cis, function(x) x[1:max(lengths(plot$cis))])))
+  
+  prepDF <- data.frame(
+    plot$labels,
+    plot$topics,
+    unlist(plot$means),
+    plotDF$X2.5.,
+    plotDF$X97.5.)
+
+  colnames(prepDF) <- c("primary_topic",
+                        "topic_n",
+                        "mean",
+                        "lower",
+                        "upper")
+
+  prepDF <- prepDF %>%
+    group_by(primary_topic) %>%
+    summarise(
+      avg_ci_point_estimate = mean(mean),
+      avg_ci_lower_bound = mean(lower),
+      avg_ci_upper_bound = mean(upper))
+
+  table_text <- cbind(
+    c(NA, seq(1, length(prepDF$primary_topic))),
+    c("Primary Topic", as.vector(prepDF$primary_topic)),
+    c("Confidence Interval (95%)", formatInterval(
+      prepDF$avg_ci_point_estimate,
+      prepDF$avg_ci_lower_bound,
+      prepDF$avg_ci_upper_bound)))
+
+  pdf(str_c("~/Documents/", file_name))
+  forestplot(table_text,
+             graph.pos = 3,
+             is.summary = c(TRUE, rep(FALSE, length(prepDF$primary_topic))),
+             align = c("r", "l", "r"),
+             mean = c(NA, prepDF$avg_ci_point_estimate),
+             lower = c(NA, prepDF$avg_ci_lower_bound),
+             upper = c(NA, prepDF$avg_ci_upper_bound),
+             fn.ci_norm = fpDrawCircleCI,
+             hrzl_lines = list("2" = gpar(lwd = 2, col = "#000000")),
+             boxsize = .15,
+             xlab = (str_c("\n", covar_label_1, " ... ", covar_label_2)),
+             cex = 0,
+             zero = 0,
+             new_page = FALSE,
+             txt_gp = fpTxtGp(
+               xlab = gpar(cex = .75),
+               label = gpar(cex = .75),
+               ticks = gpar(cex = .75)
+             ),
+             col = fpColors(box = "black", lines = "black", zero = "gray50"),
+             cex = 0.9, lineheight = "auto", colgap = unit(4, "mm"),
+             lwd.ci = 1,
+             clip = c(-.1, 1)
+  )
+  dev.off()
+  
+}
+
 # PLOT _
 # Plot interest effects
 prepInterests <- estimateEffect(
@@ -222,16 +295,18 @@ prepInterests <- estimateEffect(
   uncertainty = "Global"
 )
 pdf("~/Documents/thesis/data/figures/effects_interests.pdf")
-plot(prepInterests,
-  covariate = "Interests", topics = c(3, 8, 20, 37),
-  model = stm, method = "difference",
-  cov.value1 = "Right Wing", cov.value2 = "Left Wing",
-  xlab = "Left wing ... Right wing",
-  main = "",
-  labeltype = "custom",
-  xlim = c(-.15, .13),
-  custom.labels = c("Black Empowerment", "Race Tensions", "Patriotism", "Election")
-)
+
+plotInterests <- plot(prepInterests,
+                      covariate = "Interests", 
+                      topics = c(1:49),
+                      model = stm, 
+                      method = "difference",
+                      cov.value1 = "Right Wing", 
+                      cov.value2 = "Left Wing",
+                      xlab = "Left wing ... Right wing",
+                      main = "",
+                      labeltype = "custom",
+                      custom.labels = topicNames)
 dev.off()
 
 pdf("~/Documents/thesis/data/figures/effects_interests_3_8_20_37.pdf")
@@ -614,25 +689,13 @@ clusterPrepDf <- clusterPrepDf %>%
     avg_ci_upper_bound = mean(upper)
   )
 
-formatInterval <- function(mean, lower, upper) {
-  upper <- formatC(upper, format = "f", digits = 3)
-  upper <- ifelse(grepl("-", upper), str_c("", upper), str_c("  ", upper))
-  lower <- formatC(lower, format = "f", digits = 3)
-  lower <- ifelse(grepl("-", lower), str_c(" ", lower), str_c("   ", lower))
-  interval <- str_c("[", lower, " , ", upper, " ]")
-  mean <- formatC(mean, format = "f", digits = 3)
-  return(str_c(mean, "   ", interval))
-}
-
 table_text <- cbind(
   c(NA, seq(1, length(clusterPrepDf$primary_topic))),
   c("Primary Topic", as.vector(clusterPrepDf$primary_topic)),
   c("Confidence Interval (95%)", formatInterval(
     clusterPrepDf$avg_ci_point_estimate,
     clusterPrepDf$avg_ci_lower_bound,
-    clusterPrepDf$avg_ci_upper_bound
-  ))
-)
+    clusterPrepDf$avg_ci_upper_bound)))
 
 pdf("~/Documents/coolforest.pdf")
 forestplot(table_text,
