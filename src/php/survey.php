e828ee217a1af49c692ba2534cd7f59901ac0348
drewleonard <drew.n.leonard.19@dartmouth.edu>
2019-01-26 19:48:02 -0500

    updated survey src with 3072 / no styling

--- src/php/survey.php
+++ src/php/survey.php
@@ -1,41 +1,44 @@
-<?php
+<html>
+ <head>
+  <title>Survey</title>
+ </head>
+ <body>
+ <?php
 
-setlocale(LC_MONETARY, 'en_US');
+ // Settings
+ header('Content-type: text/xml');
+ setlocale(LC_MONETARY, 'en_US');
 
+ // Number of images: total
+ $nImgs = 3072;
 
-// TODO 
-// Number of images: total
-$nImgs = 3500; // TODO: get
+ // Number of images: subset
+ $nSlice = 10;
 
-// Number of images: subset
-$nSlice = 10;
+ // Get ids of images to pull
+ $idsAll = range(1, $nImgs);
+ shuffle($idsAll);
+ $idsSubset = array_slice($idsAll, 0, $nSlice);
 
-// Get ids of images to pull
-$idsAll = range(1, $nImgs);
-shuffle($idsAll);
-$idsSubset = array_slice($idsAll, 0, $nSlice);
+ // Build array of image tags (html)
+ $imgTags = [];
+ foreach($idsSubset as $id) {
 
-// Set image properties
-$imgHeight = 4400;
-$imgWidth = 3400;
+ 	$imgTag = '<img src="https://storage.cloud.google.com/thesis-ira-ads/ira_ad_'.$id.'.png"/>';
+ 	$imgTags[] = $imgTag;
 
-// Build array of image tags (html)
-$imgTags = [];
-foreach($idsSubset as $id) {
+ }
 
-	$imgTag = '<img src="https://storage.cloud.google.com/thesis-ira-ads/ira_ad_'.$id.'.png" height="'.$imgHeight.'" width="'.$imgWidth.'" style="width:'.$imgWidth.'px; height:'.$imgHeight.'px;"/>';
-	$imgTags[] = $imgTag;
+ $xmlResult = new SimpleXMLElement('<xml/>');
+ $imgTagsChild = $xmlResult->addChild('imgTagsChild');
 
-}
+ foreach($imgTags as $imgTagN=>$imgTag) {
+ 	$imgTagsChild->addChild('imgTag'.$imgTagN, $imgTag);
+ }
 
-$xmlResult = new SimpleXMLElement('<xml/>');
-$imgTagsChild = $xmlResult->addChild('imgTagsChild');
-
-foreach($imgTags as $imgTagN=>$imgTag) {
-
-	$imgTagsChild->addChild('imgTag'.$imgTagN, $imgTag);
-
-}
-
-Header('Content-type: text/xml');
-print($xmlResult->asXML());
+ ob_clean();
+ print($xmlResult->asXML());
+ $app->run();
+ ?> 
+ </body>
+</html>
